a4b5ba3fa29c02ce5a62881239af6c67c5bb775b
Jacobthecat258 <81988739+Jacobthecat258@users.noreply.github.com>
2022-02-21 17:11:18 -0500

    Homepage attempt

--- code/jtc.js
+++
@@ -1,6 +0,0 @@
-var projects = (() => {
-    var obj = await fetch("projects.json");
-    var pro = await obj.text();
-    return pro;
-})();
-console.log(projects);
---
+++ home.js
@@ -0,0 +1,6 @@
+var projects = (() => {
+    var obj = await fetch("projects.json");
+    var pro = await obj.text();
+    return pro;
+})();
+console.log(projects);
